68eb1eb5fad770268d536276b596684d4a93a4d9
JUN <soipon0@gmail.com>
2019-05-05 00:20:13 +0900

    jqueryの処理作成

--- public/index.php
+++ public/index.php
@@ -32,6 +32,8 @@ shuffle($data['a']);
         </ul>
         <div id="btn" class="disabled">Next Question</div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="quiz.js"></script>
 </body>
 
 </html>
---
+++ public/quiz.js
@@ -0,0 +1,16 @@
+$(function() {
+  "use strict";
+
+  $(".answer").on("click", function() {
+    var $selected = $(this);
+    var answer = $selected.text();
+
+    $.post("/_answer.php", {}).done(function(res) {
+      if (answer === res.correct_answer) {
+        //correct
+      } else {
+        // wrong
+      }
+    });
+  });
+});
