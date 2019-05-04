485d38aed33cc799ea39edbbcba2a5a2cd8fe11d
JUN <soipon0@gmail.com>
2019-05-04 22:15:12 +0900

    index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
-<?php
-
-echo "Hello web";
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>Quiz</title>
+</head>
+<body>
+    <div id="container">
+        <h1>Q. What is A?</h1>
+        <ul>
+            <li class="answer">A0</li>
+            <li class="answer">A1</li>
+            <li class="answer">A2</li>
+            <li class="answer">A3</li>
+        </ul>
+        <div id="btn" class="disabled">Next Question</div>
+    </div>
+</body>
+</html>
